cfaaf9fa1966964255652fca82770129e1eaa591
Luciano Resende <lresende@apache.org>
2016-10-12 14:01:11 -0700

    Add trace information for each install step

--- mesos-cluster.sh
+++ mesos-cluster.sh
@@ -45,6 +45,7 @@ echo "Nodes..............: ${NODES[@]}"
 echo ">>> "
 
 function prepareOs {
+  echo "PrepareOS - Skip yum update for $1"
   ssh -o StrictHostKeyChecking=no $1 "yum update -y"
   ssh -o StrictHostKeyChecking=no $1 "yum upgrade -y"
   #ssh -o StrictHostKeyChecking=no $1 "systemctl stop firewalld"
@@ -52,6 +53,7 @@ function prepareOs {
 }
 
 function installDocker {
+  echo "InstallDocker for $1"
   cat etc/yum.repos.d/docker.repo | ssh -o StrictHostKeyChecking=no $1 "cat > /etc/yum.repos.d/docker.repo"
 
   #ssh -o StrictHostKeyChecking=no $1 "curl -fsSL https://get.docker.com/ | sh"
@@ -76,6 +78,7 @@ function uninstallDCOS {
 }
 
 function uninstallDCOSBootstrap {
+  echo "Uninstall previous version of DCOS for $1"
   # Delete all containers
   ssh -o StrictHostKeyChecking=no $BOOTSTRAP "docker rm \$(docker ps -a -q)"
   # Delete all images
@@ -88,6 +91,7 @@ function uninstallDCOSBootstrap {
 }
 
 function uninstallDCOSNode {
+  echo "Uninstall previous version of DCOS for $1"
   ssh -o StrictHostKeyChecking=no $1 "/opt/mesosphere/bin/pkgpanda uninstall"
   ssh -o StrictHostKeyChecking=no $1 "rm -rf /opt/dcos-install"
   ssh -o StrictHostKeyChecking=no $1 "rm -rf /opt/dcos_install_tmp"
@@ -113,6 +117,7 @@ function uninstallDCOSNode {
 }
 
 function prepareDCOS {
+  echo "Prepare DCOS for $1"
   ssh -o StrictHostKeyChecking=no $1 "rm -rf /opt/dcos-install"
   ssh -o StrictHostKeyChecking=no $1 "mkdir -p /opt/dcos-install/genconf"
   ssh -o StrictHostKeyChecking=no $1 "mkdir -p /opt/dcos-install/tmp"
@@ -125,6 +130,7 @@ function prepareDCOS {
 }
 
 function installDCOS {
+  echo "Install DCOS for $1"
   ssh -o StrictHostKeyChecking=no $1 "cd /opt/dcos-install && bash dcos_generate_config.sh --genconf"
   ssh -o StrictHostKeyChecking=no $1 "cd /opt/dcos-install && bash dcos_generate_config.sh --install-prereqs"
   ssh -o StrictHostKeyChecking=no $1 "cd /opt/dcos-install && bash dcos_generate_config.sh --preflight"
@@ -133,6 +139,7 @@ function installDCOS {
 }
 
 function backupDCOS {
+  echo "Backing up DCOS install files to $1"
   ssh -o StrictHostKeyChecking=no $1 "cd /opt/dcos-install/genconf/serve && tar cf dcos-install.tar *"
   mkdir backup
   scp $1:/opt/dcos-install/genconf/serve/dcos-install.tar backup/dcos-install.tar
